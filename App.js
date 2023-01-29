1e83449fbee6267d31d63cb821e42a0306342732
Marcelo Barbosa <marcelo.castilho2707@gmail.com>
2023-01-29 12:02:42 -0300

    feat: adding fonts and changing splash screen

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as Font from 'expo-font'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false)
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      try {
+        await Font.loadAsync({
+          "black": require("./assets/fonts//Roboto-Black.ttf"),
+          "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          "bold": require("./assets/fonts/Roboto-Bold.ttf"),
+          "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          "italic": require("./assets/fonts/Roboto-Italic.ttf"),
+          "light": require("./assets/fonts/Roboto-Light.ttf"),
+          "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+          "medium": require("./assets/fonts/Roboto-Medium.ttf"),
+          "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          "regular": require("./assets/fonts/Roboto-Regular.ttf"),
+          "thin": require("./assets/fonts/Roboto-Thin.ttf"),
+          "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        })
+      } 
+      catch (error) {
+        console.log(error)
+      }
+      finally {
+        setAppIsLoaded(true)
+      }
+    };
+
+    loadFonts()
+  }, [])
+
   return (
+    <SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -17,4 +58,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontFamily: "black"
+  }
 });
